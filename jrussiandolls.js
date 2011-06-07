ed5a6cca53484b89dd0701f6a4b2da019ad12101
Gautier Hayoun <ghayoun@gmail.com>
2011-06-07 17:15:11 +0100

    Added labelName option

--- jrussiandolls.js
+++ jrussiandolls.js
@@ -1,30 +1,7 @@
 /**
- * Copyright 2010 Gautier Hayoun. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are
- * permitted provided that the following conditions are met:
- * 
- *    1. Redistributions of source code must retain the above copyright notice, this list of
- *       conditions and the following disclaimer.
- * 
- *    2. Redistributions in binary form must reproduce the above copyright notice, this list
- *       of conditions and the following disclaimer in the documentation and/or other materials
- *       provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY Gautier Hayoun ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Gautier Hayoun OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * Documentation
+ * Copyright 2010 Gautier Hayoun.
  *
- * TODO
- *
+ * See README for documentation
  */
 (function(jQuery) {
   jQuery.fn.russiandolls = function(options) {
@@ -47,7 +24,7 @@
                     data = data[opts.extractField];
                   }
                   select.append("<option value=" +data.id+ ">"
-                                  +data.label+"</option>");
+                                  +data[opts.labelName]+"</option>");
                 });
               } else {
                 select.append("<option value=''>" + opts.emptyMsg + "</option>");
@@ -62,6 +39,7 @@
     url: "/data.json",
     paramName: "filter",
     extractField: null,
+    labelName: "label",
     child: null,
     emptyMsg: "No data found"
   };
